5fa7f71f8f861a35a6c4e8cc127b6312d970345c
diego64 <diegoferreira1964@gmail.com>
2024-05-24 20:42:58 -0300

    Removing the files :fire:

--- src/users/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller()
-export class UserController {
-  @Get('/helloUser')
-  helloUser() {
-    return 'Seja bem vindo(a) ao nosso sistema';
-  }
-}
