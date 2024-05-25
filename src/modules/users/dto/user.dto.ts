1e45287d7fc17ee3297692995bd9f1bf18beb848
diego64 <diegoferreira1964@gmail.com>
2024-05-25 17:48:45 -0300

    Decoupling the repository layer :recycle:

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -4,3 +4,13 @@ export type CreateUserDTO = {
   username: string;
   password: string;
 };
+
+export type UserCreatedDTO = {
+  id: string;
+  createdAt: Date;
+} & CreateUserDTO;
+
+export type UsernameAndEmail = {
+  email: string;
+  username: string;
+};
---
+++ src/modules/users/repositories/prisma/user.prisma.repository.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/infra/database/prisma.service';
+import {
+  UsernameAndEmail,
+  UserCreatedDTO,
+  CreateUserDTO,
+} from '../../dto/user.dto';
+import { IUserRepository } from '../user.repository';
+
+@Injectable()
+export class UserPrismaRepository implements IUserRepository {
+  constructor(private prisma: PrismaService) {}
+
+  async findByUsernameOrEmail(data: UsernameAndEmail): Promise<UserCreatedDTO> {
+    return await this.prisma.user.findFirst({
+      where: {
+        OR: [{ username: data.username }, { email: data.email }],
+      },
+    });
+  }
+
+  async save(data: CreateUserDTO): Promise<UserCreatedDTO> {
+    return await this.prisma.user.create({
+      data,
+    });
+  }
+
+  async findByUsername(username: string): Promise<UserCreatedDTO> {
+    return await this.prisma.user.findUnique({
+      where: {
+        username: data.username,
+      },
+    });
+  }
+}
