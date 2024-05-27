0b9c5268b89594e384e00979611071f23292c9d4
diego64 <diegoferreira1964@gmail.com>
2024-05-27 18:45:09 -0300

    Configuring UseGuards for user authentication :lock:

---
+++ src/infra/providers/auth-guard.provider.ts
@@ -0,0 +1,38 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request } from 'express';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const payload = await this.jwtService.verifyAsync(token, {
+        secret: process.env.JWT_SECRET,
+      });
+      console.log(payload);
+    } catch {
+      throw new UnauthorizedException();
+    }
+
+    return true;
+  }
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const typeToken = 'Bearer';
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === typeToken ? token : undefined;
+  }
+}
